94a840e56ea9a16012184b36d5d229349dfed246
GeneralShark <austinhorton289@gmail.com>
2023-03-29 21:39:18 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,31 +1,7 @@
 Config = {}
 
-Config.Ammunition = {
-	['box-ammo-22'] = {ammo = 'ammo-22', amount = 32},
-	['box-ammo-38'] = {ammo = 'ammo-38', amount = 32},
-	['box-ammo-44'] = {ammo = 'ammo-44', amount = 32},
-	['box-ammo-9'] = {ammo = 'ammo-9', amount = 32},
-	['box-ammo-45'] = {ammo = 'ammo-45', amount = 20},
-	['box-ammo-50'] = {ammo = 'ammo-50', amount = 20},
-	['box-ammo-shotgun'] = {ammo = 'ammo-shotgun', amount = 24},
-	['box-ammo-rifle'] = {ammo = 'ammo-rifle', amount = 20},
-	['box-ammo-rifle2'] = {ammo = 'ammo-rifle2', amount = 20},
-	['box-ammo-sniper'] = {ammo = 'ammo-sniper', amount = 20},
-	['box-ammo-musket'] = {ammo = 'ammo-musket', amount = 20},
-}
-
--- Drug Bricks --
-Config.drugBrick = {
-	['meth_brick'] = 'meth_baggie',
-	['coke_brick'] = 'cocaine_packaged',
-	['xtc_brick'] = 'xtc_baggie',
-	['weed_brick'] = 'weed',
-}
-
-
---- NIK KIT --
 Config.NIK = {
-		Amphetamine = {
+	Amphetamine = {
 		'meth_baggie',
 		'meth_brick',
 		'meth_pure'
